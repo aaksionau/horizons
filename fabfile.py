6b952e05483feffe9b2a439b7f6f12c1a542dcb5
Aliaksei Aksionau <alexei.aksenov@gmail.com>
2019-04-11 13:30:47 +0300

    fixing path to .restart

--- fabfile.py
+++ fabfile.py
@@ -4,7 +4,7 @@ from fabric.api import abort, cd, env, local, run, task
 env.use_ssh_config = True
 env.hosts = ["webfaction"]
 env.remote_app_dir = '/home/paloni/webapps/horizons/'
-env.remote_apache_dir = '/home/paloni/webapps/horizons/apache2/bin/'
+env.remote_apache_dir = '/home/paloni/webapps/horizons/apache2/bin'
 
 
 @task
